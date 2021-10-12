e77ad362410161580d77c073429840ae220a901c
Dexlysic <dexlysichd@gmail.com>
2021-10-12 10:38:37 -0500

    Completed game logic.  Add'l error handling cases.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -3,21 +3,40 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const enteredLifeAmount = +prompt('Set maximum life:', 100);
+
+if (isNaN(enteredLifeAmount) || enteredLifeAmount <= 0) {
+    chosenMaxLife = 100;
+}
+
+let hasBonusLife = true;
+let chosenMaxLife = enteredLifeAmount;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
 function endRound() {
+    const initialPlayerHealth = currentPlayerHealth;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
+
+    if (currentPlayerHealth <= 0 && hasBonusLife) {
+        hasBonusLife = false;
+        removeBonusLife();
+        currentPlayerHealth = initialPlayerHealth;
+        alert('You fall, but your bonus life brings you back into the fight!')
+    }
     if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
         alert('The monster has defeated you...')
     } else if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert('You defeated the monster!')
     } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
         alert('Both you and the monster pass out from the exhausting fight.  Draw!')
+    }
+
+    if (currentPlayerHealth <= 0 || currentMonsterHealth <= 0) {
+        reset();
     }
     return;
 }
@@ -53,6 +72,12 @@ function healPlayerHandler() {
     increasePlayerHealth(healValue);
     currentPlayerHealth += healValue;
     endRound();
+}
+
+function reset() {
+    currentMonsterHealth = chosenMaxLife;
+    currentPlayerHealth = chosenMaxLife;
+    resetGame(chosenMaxLife);
 }
 
 attackBtn.addEventListener('click', attackHandler)
